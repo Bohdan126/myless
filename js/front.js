25e01e4d74c41635fc7a77d4527c1e46f1ccb913
bohdan126 <bohdan126@gmail.com>
2019-11-24 21:05:41 +0200

    added owlcarousel

--- js/front.js
+++ js/front.js
@@ -1,21 +1,47 @@
-(function ($, Drupal) {
-
-  Drupal.behaviors.initSlider = {
+(function ($) {
+  Drupal.behaviors.drupalBookOwlCarousel = {
     attach: function (context, settings) {
-      $('.view-articles-block-slider .view-content').slick({
-        dots: true,
-        speed: 800,
-        autoplay: true,
-        arrows: false,
+      $('.view-articles-block-slider .view-content').owlCarousel({
+        items:4,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true,
+        responsiveClass:true,
+        responsive:{
+          0: {
+            items: 1
+          },
+          600:{
+            items: 2
+          },
+          1000:{
+            items: 3
+          }
+        }
       });
-      $('.view-basic-page-block-slider .view-content').slick({
-        speed: 300,
-        infinite: false,
-        arrows: true,
-        slidesToShow: 6,
-        slidesToScroll: 6,
-      });
+      $('.view-articles-block-slider .view-content').addClass('owl-carousel');
+
+      $('.view-basic-page-block-slider .view-content').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+          0:{
+            items:1
+          },
+          600:{
+            items:3
+          },
+          1000:{
+            items:5
+          }
+        }
+      })
     }
   };
+  $('.view-basic-page-block-slider .view-content').addClass('owl-carousel');
 
-})(jQuery, Drupal);
+})(jQuery);
+
